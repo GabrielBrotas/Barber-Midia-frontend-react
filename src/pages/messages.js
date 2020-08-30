60cd60088ee6eded7b332fea6c4422d0404a8ea9
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-08-30 15:50:11 -0300

    Update messages.js

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import Grid from '@material-ui/core/Grid'
+
 export default function messages() {
     return (
-        <div>
-            chat pagee
-        </div>
-    )
-}
+    <Grid container spacing={3}>
+            
+        <Grid item sm={4} xs={12}>
+            <p>usuarios</p>
+        </Grid>
+
+        <Grid item sm={8} xs={12}>
+            <p>inbox</p>
+        </Grid>  
+        
+    </Grid>
+    
+)}
